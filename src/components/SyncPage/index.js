58fb582a90affe44d319c8c4ca34b2baa2033dc6
Dan Hemerlein <danhemerlein@gmail.com>
2021-06-04 16:45:10 -0400

    add some code lol

--- src/components/SyncPage/index.js
+++ src/components/SyncPage/index.js
@@ -1,25 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  box-shadow: inset 0 0 100px rgba(0, 0, 20, 0.7), 0 5px 15px rgba(0, 0, 0, 0.5);
-  background: -webkit-linear-gradient(
-      top,
-      rgba(255, 145, 0, 0.2) 0%,
-      rgba(255, 230, 48, 0.2) 80%
-    ),
-    -webkit-linear-gradient(20deg, rgba(255, 0, 0, 0.5) 0%, rgba(255, 0, 0, 0)
-          70%);
-  width: 100%;
-  height: 100%;
-  -webkit-filter: sepia(0.1) brightness(1.1) contrast(1);
-  transition: -webkit-filter 0.3s ease-in-out;
-  z-index: -1;
-
-  background-position: center;
-  background-repeat: no-repeat;
-`
+import './SyncPage.scss'
 
 export default function SyncPage() {
-  return <Container>this is the sync page</Container>
+  return (
+    <div className='SyncPage'>
+      <div className='HomePage__header w100 flex justify-between'>
+        <div className='HomePage__header--name'>
+          <div className='p1 flex items-center flex-col'>
+            <h1 className='HomePage__headline title-sans-serif'>
+              <span className='HomePage__first-name inline-block  color-white'>
+                JACKSON
+              </span>
+              <span className='HomePage__last-name inline-block color-white'>
+                &nbsp;HOFFMAN
+              </span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
